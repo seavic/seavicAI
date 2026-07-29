9b215dd565bf59a4461cad8f2492fb7677f4f8a3
seavic888 <seavic88@gmail.com>
2026-07-29 11:16:53 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://staging.seavicai.com",
-  base: "/seavicAI",
   output: "static",
   trailingSlash: "always",
   build: {
